522bb0a80282092bf5b9a04020facfe6fc406b6f
barbyortega <134418586+barbyortega@users.noreply.github.com>
2024-05-10 08:59:30 -0400

    consumiendo api

---
+++ actividad consumiendo api/js.js
@@ -0,0 +1,16 @@
+function validar() {
+    $(document).ready(
+        $("#enviar").click(function() {
+            $.get("https://www.themealdb.com/api/json/v1/1/categories.php",
+                function (data) {
+                    $.each(data.categories,function(i,item) {
+                        $("#categorias").append("<tr><td>"
+                        + item.idCategory + "<tr><td>"
+                        + item.strCategory + "</td><td><img src = '"
+                        + item.strCategoryThumb + "'>" + "</td><td>"
+                        + item.strCategoryDescription + "</td><tr>");
+                    })
+            })
+        })
+    )
+}
